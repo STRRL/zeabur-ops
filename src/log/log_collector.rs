3a7c71b38a99d3b9821917c20b00dd9a69fb536a
STRRL <im@strrl.dev>
2024-09-21 20:16:42 -0700

    feat: Refactor log collector module and integrate with Zeabur API

--- src/log/log_collector.rs
+++ src/log/log_collector.rs
@@ -1,11 +1,5 @@
 use anyhow::Error;
 use async_trait::async_trait;
-use chrono::{DateTime, Utc};
-use std::sync::Arc;
-use tokio::sync::Mutex;
-
-use crate::zeabur::client::ZeaburClient;
-
 use super::log_entry::LogEntry;
 
 #[async_trait]
